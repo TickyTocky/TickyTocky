39150435c4cc69d1649b87bd180d9e2001eefb55
grapefruit13 <liam.heo11@gmail.com>
2024-01-25 15:35:14 +0900

    chore: Begin project

---
+++ src/services/localStorage.ts
@@ -0,0 +1,31 @@
+class LocalStorage {
+  constructor() {}
+
+  static setItem(key: string, value: string) {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(key, value);
+    }
+  }
+
+  static getItem(key: string) {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem(key);
+    }
+
+    return null;
+  }
+
+  static removeItem(key: string) {
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem(key);
+    }
+  }
+
+  static clearItem() {
+    if (typeof window !== 'undefined') {
+      localStorage.clear();
+    }
+  }
+}
+
+export default LocalStorage;
