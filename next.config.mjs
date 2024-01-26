e328dd8d33be83b0186cec0fc8f88c6fde85ef15
designsoo <kheesoo10@gmail.com>
2024-01-26 15:05:20 +0900

    chore: Add SCSS compiling settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    additionalData: `@import "@/styles/main.scss";`,
+  },
 };
 
 export default nextConfig;
