bb5bc79e99b694c79ae14dd19b048e228364bc21
grapefruit13 <liam.heo11@gmail.com>
2024-01-28 00:52:44 +0900

    feat: Add svg files for Nav component

---
+++ src/constants/importImage.js
@@ -0,0 +1,31 @@
+import IconAddActive from 'public/svg/ic-add-active-m.svg';
+import IconLogout from 'public/svg/ic-logout.svg';
+import IconLogoutHover from 'public/svg/ic-logout-hover.svg';
+import IconNavActive from 'public/svg/ic-nav-active.svg';
+
+export const Icon = {
+  add: {
+    active: {
+      url: IconAddActive,
+      alt: 'icon-add-active-m',
+    },
+  },
+  logout: {
+    default: {
+      url: IconLogout,
+      alt: 'icon-logout',
+      src: '/svg/ic-logout.svg',
+    },
+    hover: {
+      url: IconLogoutHover,
+      alt: 'icon-logout-hover-m',
+      src: '/svg/ic-logout-hover.svg',
+    },
+  },
+  nav: {
+    active: {
+      url: IconNavActive,
+      alt: 'icon-nav-active',
+    },
+  },
+};
