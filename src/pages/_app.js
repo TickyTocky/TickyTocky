86ca33d43b6ef09813a17c108814d33e497aab79
designsoo <kheesoo10@gmail.com>
2024-01-28 21:04:58 +0900

    feat: Apply formProvider to use react-hook-form globally

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { FormProvider, useForm } from 'react-hook-form';
 import '@/styles/base/common.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const methods = useForm({ mode: 'all' });
+
+  return (
+    <FormProvider {...methods}>
+      <Component {...pageProps} />
+    </FormProvider>
+  );
 }
