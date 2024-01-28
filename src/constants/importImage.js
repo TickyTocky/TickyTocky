57f298dde139f0ac851d4983f5a610e16727b7a6
grapefruit13 <liam.heo11@gmail.com>
2024-01-28 21:46:46 +0900

    feat: Add delete svg file for modal components

---
+++ src/constants/importImage.js
@@ -0,0 +1,13 @@
+import IconDelete from 'public/svg/ic-delete.svg';
+import IconLogout from 'public/svg/ic-logout.svg';
+
+export const Icon = {
+  delete: {
+    url: IconDelete,
+    alt: 'icon-delete',
+  },
+  logout: {
+    url: IconLogout,
+    alt: 'icon-logout-default',
+  },
+};
