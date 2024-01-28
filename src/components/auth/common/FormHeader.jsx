fade4ecb7ee9fd368b77506e6f1b9219bbaa9761
designsoo <kheesoo10@gmail.com>
2024-01-28 21:06:19 +0900

    feat: Implement common form header component

---
+++ src/components/auth/common/FormHeader.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './FormHeader.module.scss';
+import classNames from 'classnames/bind';
+
+import { ICON } from '@/constants';
+
+const cx = classNames.bind(styles);
+const { logo } = ICON;
+
+const FormHeader = () => {
+  return (
+    <header className={cx('login-header')}>
+      <h1 className={cx('login-header-title')}>
+        <Link href={'/'}>
+          <Image src={logo.url} alt={logo.alt} width={257} height={40}></Image>
+        </Link>
+      </h1>
+      <p className={cx('login-header-desc')}>Manage your projects more powerful</p>
+    </header>
+  );
+};
+
+export default FormHeader;
