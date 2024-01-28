42c5135154ac4cfa33f964bc29fbedd984d168f6
suk3952111 <suk3952111@naver.com>
2024-01-28 18:28:54 +0900

    feat: Add up and down arrow svg for dropdown component

---
+++ src/constants/importImage.js
@@ -0,0 +1,15 @@
+import IconArrowDownMl from 'public/svg/ic-arrow-down-m.svg';
+import IconArrowUpMl from 'public/svg/ic-arrow-up-m.svg';
+
+export const Icon = {
+  dropdownarrow: {
+    default: {
+      url: IconArrowDownMl,
+      alt: 'ic-arrow-down-m',
+    },
+    active: {
+      url: IconArrowUpMl,
+      alt: 'ic-arrow-down-m',
+    },
+  },
+};
