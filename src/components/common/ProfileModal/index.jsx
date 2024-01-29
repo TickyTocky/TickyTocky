fa71507bdd3015312ffd30c415cff410f942b37a
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-01-30 02:31:59 +0900

    refector: Add BaseButton component and Remove event

--- src/components/common/ProfileModal/index.jsx
+++ src/components/common/ProfileModal/index.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react';
-
-import styles from './ProfileModal.module.scss';
 import classNames from 'classnames/bind';
-
-import Avatar from '@/components/common/avatar';
+import styles from './ProfileModal.module.scss';
+import Avatar from '@/components/common/Avatar';
+import BaseButton from '@/components/common/button/BaseButton';
 
 const cx = classNames.bind(styles);
 
-const ProfileModal = ({ profileName, profileImage, profileEmail }) => {
-  const [isHover, setIsHover] = useState(false);
-
-  console.log(isHover);
-
-  return (
-    <div
-      className={cx('container')}
-      onMouseOver={() => {
-        setIsHover(true);
-      }}
-      onMouseLeave={() => {
-        setIsHover(false);
-      }}
-    >
-      <div className={cx('profile')}>
-        <Avatar profileImage={profileImage} avatarSize={'xl'} />
-        <span className={cx('name')}>{profileName}</span>
-        <span className={cx('email')}>{profileEmail}</span>
-      </div>
-      <div></div>
+const ProfileModal = ({ profileName, profileImage, profileEmail }) => (
+  <div className={cx('container')}>
+    <div className={cx('profile')}>
+      <Avatar profileImage={profileImage} avatarSize={'xl'} />
+      <span className={cx('name')}>{profileName}</span>
+      <span className={cx('email')}>{profileEmail}</span>
     </div>
-  );
-};
+    <div className={cx('button-wrap')}>
+      <BaseButton size={'xl'} variant={'outline'} type={'button'} text={'My Page'} />
+      <BaseButton size={'xl'} variant={'ghost'} type={'button'} text={'Logout'} />
+    </div>
+  </div>
+);
 
 export default ProfileModal;
