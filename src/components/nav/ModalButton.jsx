12781509fe16d1f96eb0dc9f1cd96b511e1131aa
grapefruit13 <liam.heo11@gmail.com>
2024-01-30 02:13:34 +0900

    refactor: Change classname, Image src, alt and scss file name

--- src/components/nav/ModalButton.jsx
+++ src/components/nav/ModalButton.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import classNames from 'classnames/bind';
-import { Icon } from '@/constants/importImage';
+import { ICON } from '@/constants/importImage';
 import styles from '@/components/nav/navButton.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,28 +8,28 @@ const cx = classNames.bind(styles);
 const ModalButton = ({ type, onClickInput }) => {
   return (
     <button
-      className={cx('modal-btn-wrapper', { logout: type === 'logout' })}
+      className={cx('modal-button-wrapper', { logout: type === 'logout' })}
       onClick={onClickInput}
     >
       {type === 'add' && (
         <Image
-          src={Icon.add.active.url}
-          alt={Icon.add.active.alt}
+          src={ICON.add.active.url}
+          alt={ICON.add.active.alt}
           width={24}
           height={24}
         />
       )}
       {type === 'logout' && (
         <Image
-          src={Icon.logout.default.url}
-          alt={Icon.logout.default.alt}
+          src={ICON.logout.default.url}
+          alt={ICON.logout.default.alt}
           width={24}
           height={24}
           onMouseOver={(e) => {
-            return (e.currentTarget.src = Icon.logout.hover.src);
+            return (e.currentTarget.src = ICON.logout.hover.src);
           }}
           onMouseOut={(e) => {
-            return (e.currentTarget.src = Icon.logout.default.src);
+            return (e.currentTarget.src = ICON.logout.default.src);
           }}
         />
       )}
