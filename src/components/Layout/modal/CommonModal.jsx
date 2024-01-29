88fdda4ccf7e581716fd5723bbbe2b96a3f5fec8
grapefruit13 <liam.heo11@gmail.com>
2024-01-29 19:23:29 +0900

    refactor: Integrate CardModal and CommonModal to CommonModal

--- src/components/Layout/modal/CommonModal.jsx
+++ src/components/Layout/modal/CommonModal.jsx
@@ -1,22 +1,38 @@
 import ReactModal from 'react-modal';
 import classNames from 'classnames/bind';
-import styles from '@/components/Layout/Modal/modal.module.scss';
+import styles from './modal.module.scss';
 
 const cx = classNames.bind(styles);
 
-const CommonModal = ({ size = '', label = '', isCommonModalOpened, children }) => {
-  return (
-    <ReactModal
-      isOpen={isCommonModalOpened}
-      className={cx('common-modal-container', {
-        'common-modal-lg': size === 'lg',
-      })}
-      overlayClassName={cx('modal-overlay')}
-    >
+const CommonModal = ({
+  isModalOpen,
+  closeModal,
+  children,
+  label = '',
+  isDetail = false,
+  detailInfo = { columnTitle: 'To do', cardTitle: 'Side Project - Taskify' },
+}) => (
+  <ReactModal
+    isOpen={isModalOpen}
+    onRequestClose={closeModal}
+    shouldCloseOnOverlayClick={true}
+    className={cx('common-modal-container')}
+    overlayClassName={cx('modal-overlay')}
+    contentLabel='common-modal'
+  >
+    {isDetail ? (
+      <div className={cx('modal-header-container')}>
+        <div className={cx('title-container')}>
+          <div className={cx('column-title')}>{detailInfo.columnTitle}</div>
+          <div className={cx('card-title')}>{detailInfo.cardTitle}</div>
+        </div>
+        <div>케밥버튼</div>
+      </div>
+    ) : (
       <label className={cx('label')}>{label}</label>
-      {children}
-    </ReactModal>
-  );
-};
+    )}
+    {children}
+  </ReactModal>
+);
 
 export default CommonModal;
