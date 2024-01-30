0cdc689d4fdf58214134fe4c777fad4ef667ede8
suk3952111 <suk3952111@naver.com>
2024-01-31 00:35:24 +0900

    chore: Delete wrongly commited files

--- src/components/common/TagsInput/ParentTagsInput.jsx
+++
@@ -1,9 +0,0 @@
-import TagField from '.';
-import { useState } from 'react';
-
-const ParentTagsInput = () => {
-  const [tagList, setTagList] = useState([]);
-  return <TagField tagList={tagList} setTagList={setTagList} />;
-};
-
-export default ParentTagsInput;
