d99497c22db9481af777211bac8f90dbf0dcb546
grapefruit13 <liam.heo11@gmail.com>
2024-01-30 17:59:34 +0900

    refactor: Set onClickInput prop's initial value to null and fix typo

--- src/components/nav/ModalButton.jsx
+++ src/components/nav/ModalButton.jsx
@@ -1,40 +1,29 @@
 import Image from 'next/image';
 import classNames from 'classnames/bind';
 import { ICON } from '@/constants/importImage';
-import styles from '@/components/nav/navButton.module.scss';
+import styles from './navButton.module.scss';
 
 const cx = classNames.bind(styles);
 
-const ModalButton = ({ type, onClickInput }) => {
-  return (
-    <button
-      className={cx('modal-button-wrapper', { logout: type === 'logout' })}
-      onClick={onClickInput}
-    >
-      {type === 'add' && (
-        <Image
-          src={ICON.add.active.url}
-          alt={ICON.add.active.alt}
-          width={24}
-          height={24}
-        />
-      )}
-      {type === 'logout' && (
-        <Image
-          src={ICON.logout.default.url}
-          alt={ICON.logout.default.alt}
-          width={24}
-          height={24}
-          onMouseOver={(e) => {
-            return (e.currentTarget.src = ICON.logout.hover.src);
-          }}
-          onMouseOut={(e) => {
-            return (e.currentTarget.src = ICON.logout.default.src);
-          }}
-        />
-      )}
-    </button>
-  );
-};
+const ModalButton = ({ type, onClickInput = null }) => (
+  <button
+    className={cx('modal-button-wrapper', { logout: type === 'logout' })}
+    onClick={onClickInput}
+  >
+    {type === 'add' && (
+      <Image src={ICON.add.active.url} alt={ICON.add.active.alt} width={24} height={24} />
+    )}
+    {type === 'logout' && (
+      <Image
+        src={ICON.logout.default.url}
+        alt={ICON.logout.default.alt}
+        width={24}
+        height={24}
+        onMouseOver={(e) => (e.currentTarget.src = ICON.logout.hover.src)}
+        onMouseOut={(e) => (e.currentTarget.src = ICON.logout.default.src)}
+      />
+    )}
+  </button>
+);
 
 export default ModalButton;
