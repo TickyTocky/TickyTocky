2a1e1887b9e6dcd1d91dfd183e84814677843b0d
grapefruit13 <liam.heo11@gmail.com>
2024-01-30 17:58:04 +0900

    refactor: Fix typo and remove passing  onClickInput props to children button components

--- src/components/Layout/Nav/index.jsx
+++ src/components/Layout/Nav/index.jsx
@@ -1,31 +1,29 @@
 import classNames from 'classnames/bind';
 import ModalButton from '@/components/nav/ModalButton';
 import LinkButton from '@/components/nav/LinkButton';
-import useNavModalStore from '@/stores/useNavModalStore';
-import styles from '@/components/Layout/Nav/nav.module.scss';
 import mock from '@/constants/navMockData';
+import styles from './nav.module.scss';
 
 const cx = classNames.bind(styles);
 
 const Nav = () => {
-  const { dashboardAdd, clickLogout } = useNavModalStore();
-  const boards = mock.dashboards;
+  const { dashboards } = mock;
 
   return (
     <div className={cx('nav-container')}>
       <div className={cx('button-container')}>
-        <div className={cx('devider')}>
-          <ModalButton type='add' onClickInput={dashboardAdd} />
+        <div className={cx('divider')}>
+          <ModalButton type='add' />
         </div>
         <div className={cx('home-dashboard-container')}>
           <LinkButton type='home' />
-          {boards.map((board) => {
-            return <LinkButton key={board.id} type='board' boardData={board} />;
-          })}
+          {dashboards.map((board) => (
+            <LinkButton key={board.id} type='board' boardData={board} />
+          ))}
         </div>
       </div>
       <div className={cx('logout-container')}>
-        <ModalButton type='logout' onClickInput={clickLogout} />
+        <ModalButton type='logout' />
       </div>
     </div>
   );
