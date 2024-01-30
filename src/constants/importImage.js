a811faa688f0cc8b2b39140c834b19bb82f4c238
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-01-30 17:48:49 +0900

    feat: Add svg files

--- src/constants/importImage.js
+++ src/constants/importImage.js
@@ -1,5 +1,7 @@
 import IconDelete from 'public/svg/ic-delete.svg';
 import IconLogout from 'public/svg/ic-logout.svg';
+import IconArrowRight from 'public/svg/ic-arrow-right.svg';
+import IconNav from 'public/svg/ic-nav.svg';
 
 export const ICON = {
   delete: {
@@ -9,5 +11,17 @@ export const ICON = {
   logout: {
     url: IconLogout,
     alt: 'icon-logout-default',
+  },
+  arrow: {
+    right: {
+      url: IconArrowRight,
+      alt: 'icon-arrow-right',
+    },
+  },
+  nav: {
+    default: {
+      url: IconNav,
+      alt: 'icon-nav',
+    },
   },
 };
