e0bd17cce00dc8fbab01a442d608ce8b628f422e
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-01-30 11:41:41 +0900

    refactor: Add MixButton component

--- src/components/common/InvitationMemebers/index.jsx
+++ src/components/common/InvitationMemebers/index.jsx
@@ -1,44 +1,37 @@
-import { useEffect } from 'react';
-import styles from './InvitationMembers.module.scss';
 import classNames from 'classnames/bind';
+import styles from './InvitationMembers.module.scss';
 import useInvitationMembers from '@/hooks/useInvitationMembers';
-import Avatar from '@/components/common/avatar';
+import Avatar from '@/components/common/Avatar';
+import MixButton from '@/components/common/button/MixButton';
+import { Icon } from '@/constants/importImage';
 
 const cx = classNames.bind(styles);
 
 const InvitationMembers = ({ members }) => {
-  const { visibleMembersNum, handleInvitationMembers } = useInvitationMembers();
-
-  useEffect(() => {
-    handleInvitationMembers();
-  }, [visibleMembersNum, handleInvitationMembers]);
+  const { visibleMembersNum } = useInvitationMembers();
 
   return (
     <div className={cx('container')}>
       <ul className={cx('members-list')}>
         {members.length > visibleMembersNum
-          ? members.slice(0, visibleMembersNum).map((member) => {
-              return (
-                <li key={member.id}>
-                  <Avatar
-                    profileName={member.nickname}
-                    profileImage={member.profileImageUrl}
-                    avatarSize={'m'}
-                  />
-                </li>
-              );
-            })
-          : members.map((member) => {
-              return (
-                <li key={member.id}>
-                  <Avatar
-                    profileName={member.nickname}
-                    profileImage={member.profileImageUrl}
-                    avatarSize={'m'}
-                  />
-                </li>
-              );
-            })}
+          ? members.slice(0, visibleMembersNum).map((member) => (
+              <li key={member.id}>
+                <Avatar
+                  profileName={member.nickname}
+                  profileImage={member.profileImageUrl}
+                  avatarSize={'md'}
+                />
+              </li>
+            ))
+          : members.map((member) => (
+              <li key={member.id}>
+                <Avatar
+                  profileName={member.nickname}
+                  profileImage={member.profileImageUrl}
+                  avatarSize={'md'}
+                />
+              </li>
+            ))}
         {members.length > visibleMembersNum && (
           <li>
             <div className={cx('hidden-members-num')}>
@@ -47,6 +40,14 @@ const InvitationMembers = ({ members }) => {
           </li>
         )}
       </ul>
+      <MixButton
+        svg={Icon.add.default.url}
+        size={18}
+        type={'button'}
+        gap={4}
+        text={'Invite'}
+        fontSize={14}
+      />
     </div>
   );
 };
