438839abd4ff13e4f541f3ddc067f954e97ed9f7
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-01-31 14:16:40 +0900

    feat: Add svg files

--- src/constants/importImage.js
+++ src/constants/importImage.js
@@ -4,6 +4,9 @@ import IconLogoutHover from 'public/svg/ic-logout-hover.svg';
 import IconNavActive from 'public/svg/ic-nav-active.svg';
 import IconNav from 'public/svg/ic-nav.svg';
 import IconDelete from 'public/svg/ic-delete.svg';
+import IconArrowUp from 'public/svg/ic-arrow-up.svg';
+import IconArrowUpActive from 'public/svg/ic-arrow-up-active.svg';
+import IconArrowDown from 'public/svg/ic-arrow-down.svg';
 
 export const ICON = {
   add: {
@@ -34,8 +37,22 @@ export const ICON = {
       alt: 'icon-nav',
     },
   },
-   delete: {
+  delete: {
     url: IconDelete,
     alt: 'icon-delete',
+  },
+  arrowUp: {
+    default: {
+      url: IconArrowUp,
+      alt: 'icon-arrow-up',
+    },
+    active: {
+      url: IconArrowUpActive,
+      alt: 'icon-arrow-up-active',
+    },
+  },
+  arrowDown: {
+    url: IconArrowDown,
+    alt: 'icon-arrow-down',
   },
 };
