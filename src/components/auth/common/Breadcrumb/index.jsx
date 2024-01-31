0944987bdf0799e0044ca18cc8a6a1be38de2a1c
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-01-31 16:26:47 +0900

    feat: Implement Breadcrumb component

---
+++ src/components/auth/common/Breadcrumb/index.jsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import classNames from 'classnames/bind';
+import { ICON } from '@/constants/importImage';
+import styles from './Breadcrumb.module.scss';
+
+const cx = classNames.bind(styles);
+const { arrowRight, nav } = ICON;
+
+const Breadcrumb = ({ title }) => (
+  <div className={cx('container')}>
+    <Image src={nav.default.url} alt={nav.default.alt} width={24} height={24} />
+    <span className={cx('previous')}>Dash Boards</span>
+    <Image src={arrowRight.url} alt={arrowRight.alt} width={24} height={24} />
+    <span className={cx('current')}>{title}</span>
+  </div>
+);
+
+export default Breadcrumb;
