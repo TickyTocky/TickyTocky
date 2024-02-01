7007a2377a4740b7b01f78e1c6f21723cec59c12
suk3952111 <suk3952111@naver.com>
2024-02-01 17:51:24 +0900

    feat:Add KebabDropDown and related components

---
+++ src/components/common/DropDownList/index.jsx
@@ -0,0 +1,42 @@
+import React, { useRef, useEffect, useState } from 'react';
+import classNames from 'classnames/bind';
+import { list } from '@/constants/DropDownListData.json';
+import styles from './DropDownList.module.scss';
+
+const cx = classNames.bind(styles);
+
+const DropdownList = ({ isOpen, setIsOpen, onClickInput }) => {
+  const dropdownRef = useRef(null);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    setHeight(dropdownRef.current?.offsetHeight || 0);
+  }, [isOpen]);
+
+  const handleItemClick = (e, value) => {
+    e.stopPropagation();
+    onClickInput(value);
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={cx('dropdownlist', { close: !isOpen })}
+      style={{ maxHeight: isOpen ? `${height}px` : '0' }}
+    >
+      <ul className={cx('dropdownlist-list')} ref={dropdownRef}>
+        {list.map((item) => (
+          <li
+            className={cx('dropdownlist-list-item')}
+            key={`key-${item.id}`}
+            onClick={(e) => handleItemClick(e, item.id)}
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownList;
---
+++ src/components/common/KebabDropDown/index.jsx
@@ -0,0 +1,29 @@
+import { useRef } from 'react';
+import classNames from 'classnames/bind';
+import useDropDownDetectClose from '@/hooks/useDropDownDetectClose';
+import { ICON } from '@/constants/importImage';
+import IconButton from '@/components/common/button/IconButton';
+import DropdownList from '@/components/common/DropDownList';
+import styles from './KebabDropDown.module.scss';
+
+const cx = classNames.bind(styles);
+const { kebab } = ICON;
+
+const KebabDropDown = ({ onClickInput }) => {
+  const dropDownRef = useRef();
+  const [isOpen, setIsOpen] = useDropDownDetectClose(dropDownRef);
+
+  const handleOpenClick = (e) => {
+    e.stopPropagation();
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div className={cx('kebabdropdown')} ref={dropDownRef} onClick={handleOpenClick}>
+      <IconButton svg={kebab.url} size='md' alt={kebab.alt} outline type='button' />
+      <DropdownList isOpen={isOpen} setIsOpen={setIsOpen} onClickInput={onClickInput} />
+    </div>
+  );
+};
+
+export default KebabDropDown;
