be8bdc912a75c7fae01b587571600eba54398131
suk3952111 <suk3952111@naver.com>
2024-02-03 17:51:48 +0900

    feat: Add isDetailed prop and nowrap property

--- src/components/common/CardTags/index.jsx
+++ src/components/common/CardTags/index.jsx
@@ -3,20 +3,36 @@ import styles from './CardTags.module.scss';
 
 const cx = classNames.bind(styles);
 
-const CardTags = ({ data }) => {
-  const selectedData = data.slice(0, 4);
+const CardTags = ({ data, isDetailed = false }) => {
+  const selectedData = data.slice(0, 3);
+  const remainingData = data.slice(3, 9);
+  const MINIMUM_LENGTH = 4;
+  const TAG_MAX_LENTH = 3;
   return (
     <div className={cx('cardtags')}>
       <ul className={cx('cardtags-list')}>
         {selectedData.map((item, index) => (
           <li className={cx('cardtags-list-item')} key={`key-${index}`}>
-            {item}
+            {item.length > MINIMUM_LENGTH && !isDetailed
+              ? `${item.slice(0, MINIMUM_LENGTH)}...`
+              : item}
           </li>
         ))}
+        {!isDetailed && data.length > TAG_MAX_LENTH && (
+          <li className={cx('remaing')} key={'key-3'}>
+            +{data.length - TAG_MAX_LENTH}
+          </li>
+        )}
+        {isDetailed &&
+          remainingData.map((item, index) => (
+            <li
+              className={cx('cardtags-list-item')}
+              key={`key-${selectedData.length + index}`}
+            >
+              {item}
+            </li>
+          ))}
       </ul>
-      {data.length > 4 && (
-        <div className={cx('cardtags-remaining')}>+{data.length - 4}</div>
-      )}
     </div>
   );
 };
