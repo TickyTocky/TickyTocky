82d1bd3b2dbcb98b3c55c7c37db06123bbede17f
grapefruit13 <liam.heo11@gmail.com>
2024-02-04 16:57:24 +0900

    feat: Implement useAsync for api call

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+const useAsync = (asyncFunction, initialData, initialExecution = true) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState();
+  const [data, setData] = useState(initialData);
+
+  const execute = async () => {
+    setIsLoading(true);
+    setIsError(false);
+
+    try {
+      const res = await asyncFunction();
+      setData(res.data);
+    } catch (e) {
+      setIsError(true);
+      setError(e);
+      console.error('[API ERROR] NOT FOUND FETCH DATA', e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!initialExecution) return;
+    execute();
+  }, []);
+
+  return { isLoading, isError, data, error, execute };
+};
+
+export default useAsync;
