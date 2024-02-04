580087c78a7373622c6bbd75ccd4718d326a5db5
grapefruit13 <liam.heo11@gmail.com>
2024-02-04 19:33:37 +0900

    refactor: Update constants/index.js and add initialDataType/index.js to export all files in it

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,3 @@ export * from './auth';
 export * from './importImg';
 
 export * from './apiPaths';
-
-export * from './initialDataType/card';
-export * from './initialDataType/column';
-export * from './initialDataType/comment';
-export * from './initialDataType/dashboard';
-export * from './initialDataType/invitation';
-export * from './initialDataType/member';
-export * from './initialDataType/user';
