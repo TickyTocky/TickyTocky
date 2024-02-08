2ab39a35cfc937a31b7f70e3a71c0b9cc906df73
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-02-08 21:58:21 +0900

    feat: Extracted page name from router's pathname for improved rendering

--- src/pages/dashboard/[id]/index.jsx
+++ src/pages/dashboard/[id]/index.jsx
@@ -1,16 +1,21 @@
 import { useRouter } from 'next/router';
 import ColumnLayout from '@/components/layout/dashboard/Column';
+import Layout from '@/components/layout';
+import BoardHeader from '@/components/layout/Header/BoardHeader';
+import { members } from '@/constants/mock-assignees.json'; //TODO 임시 Mock 데이터
 
 const DashboardPage = () => {
   const router = useRouter();
-  const { id } = router.query;
+  const { query, pathname } = router;
+  const { id } = query;
+  const pageTitle = pathname.split('/')[1];
 
   if (id === undefined) return;
 
   return (
-    <>
+    <Layout boardHeader={<BoardHeader members={members} title={pageTitle} />}>
       <ColumnLayout dashBoardId={Number(id)} />
-    </>
+    </Layout>
   );
 };
 
