ec1988aec822e8da181b560ddfe1da37e032045a
designsoo <kheesoo10@gmail.com>
2024-02-08 14:27:54 +0900

    refactor: Change controller structure applied to datepicker

--- src/components/common/DateField/index.jsx
+++ src/components/common/DateField/index.jsx
@@ -1,6 +1,5 @@
+import { useFormContext, Controller } from 'react-hook-form';
 import DatePicker from 'react-datepicker';
-import { Controller } from 'react-hook-form';
-import { useFormContext } from 'react-hook-form';
 import classNames from 'classnames/bind';
 import useDatePicker from '@/hooks/useDatePicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -8,37 +7,31 @@ import styles from './DateField.module.scss';
 
 const cx = classNames.bind(styles);
 
-const DateField = ({ label, isRequired = false }) => {
+const DateField = ({ label, name, isRequired = false }) => {
   const { control } = useFormContext();
-  const { selectedDate, setSelectedDate } = useDatePicker();
+  const { selectedDate } = useDatePicker();
 
   return (
     <div className={cx('date-picker')}>
       <label className={cx('date-picker-label', { required: isRequired })}>{label}</label>
       <Controller
         control={control}
-        name='date'
+        name={name}
         defaultValue={selectedDate}
-        render={({ field }) => {
-          return (
-            <DatePicker
-              {...field}
-              calendarClassName={cx('date-picker-calendar')}
-              selected={field.value}
-              onChange={(date) => {
-                setSelectedDate(date);
-                field.onChange(date);
-              }}
-              dateFormat='yyyy. MM. dd ・ hh:mm'
-              showTimeSelect
-              timeIntervals={30}
-              timeFormat='hh:mm'
-              formatWeekDay={(nameOfDay) => {
-                return nameOfDay.substring(0, 1);
-              }}
-            />
-          );
-        }}
+        render={({ field: { value, onChange } }) => (
+          <DatePicker
+            selected={value}
+            onChange={(date) => {
+              onChange(date);
+            }}
+            showTimeSelect
+            timeFormat='HH:mm'
+            timeIntervals={30}
+            dateFormat='yyyy-MM-dd HH:mm'
+            formatWeekDay={(nameOfDay) => nameOfDay.substring(0, 1)}
+            calendarClassName={cx('date-picker-calendar')}
+          />
+        )}
       />
     </div>
   );
