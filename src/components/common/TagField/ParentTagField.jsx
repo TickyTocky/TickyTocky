c6d408174d67f6e2f675f58db72c0a9aaaefae5b
suk3952111 <suk3952111@naver.com>
2024-02-11 01:14:22 +0900

    refactor: Delete useless files

--- src/components/common/TagField/ParentTagField.jsx
+++
@@ -1,9 +0,0 @@
-import TagField from '.';
-import { useState } from 'react';
-
-const ParentTagField = () => {
-  const [tagList, setTagList] = useState([]);
-  return <TagField tagList={tagList} setTagList={setTagList} />;
-};
-
-export default ParentTagField;
