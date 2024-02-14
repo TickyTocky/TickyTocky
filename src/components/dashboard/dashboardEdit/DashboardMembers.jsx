a8bcaf118758d891f093758b4380a943c969715e
grapefruit13 <liam.heo11@gmail.com>
2024-02-14 10:20:46 +0900

    refactor: Revert DashbmardMembers

--- src/components/dashboard/dashboardEdit/DashboardMembers.jsx
+++ src/components/dashboard/dashboardEdit/DashboardMembers.jsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import BaseButton from '@/components/common/button/BaseButton';
 import Avatar from '@/components/common/Avatar';
 import DeleteDashboard from '@/components/dashboard/modal/dashboard/DeleteDashboard';
 import useModalState from '@/hooks/useModalState';
+import useAsync from '@/hooks/useAsync';
 import useMemberStore from '@/stores/useMemberStore';
+import Members from '@/api/members';
+import { INIT_MEMBER_DATA } from '@/constants/initialDataType';
 import { ICON } from '@/constants/importImage';
 import styles from './DashboardMembers.module.scss';
 
@@ -15,10 +18,11 @@ const left = page.arrowLeft;
 const right = page.arrowRight;
 
 const DashboardMembers = ({ dashboardId }) => {
+  useAsync(() => Members.getList(1, 100, Number(dashboardId)), INIT_MEMBER_DATA);
   const { memberList } = useMemberStore();
   const members = memberList?.filter((item) => !item.isOwner);
   const totalItems = members?.length;
-  const ITEMS_PER_PAGE = 6;
+  const ITEMS_PER_PAGE = 2;
 
   const [currentPage, setCurrentPage] = useState(1);
   const [currentPageData, setCurrentPageData] = useState(null);
@@ -36,12 +40,13 @@ const DashboardMembers = ({ dashboardId }) => {
       setCurrentPage((prev) => prev + 1);
     }
   };
-  const renderData = useCallback(() => {
+
+  const renderData = () => {
     const START_INDEX = (currentPage - 1) * ITEMS_PER_PAGE;
     const END_INDEX = START_INDEX + ITEMS_PER_PAGE;
     const currentPageData = members.slice(START_INDEX, END_INDEX);
     setCurrentPageData(currentPageData);
-  }, [currentPage, members, setCurrentPageData]);
+  };
 
   useEffect(() => {
     if (totalPages >= 1) {
@@ -49,13 +54,13 @@ const DashboardMembers = ({ dashboardId }) => {
     } else if (!totalPages) {
       setTotalPages(1);
     }
-  }, [totalItems, ITEMS_PER_PAGE, totalPages]);
+  }, [totalItems, ITEMS_PER_PAGE]);
 
   useEffect(() => {
     if (memberList) {
       renderData();
     }
-  }, [currentPage, memberList, renderData]);
+  }, [currentPage, memberList]);
 
   return (
     <article className={cx('container')}>
