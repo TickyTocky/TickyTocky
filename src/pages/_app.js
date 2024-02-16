d02e96b23fcd9657f754c7e685fd9c8241f4bc5e
seongseonko <kymgok1001@naver.com>
2024-02-17 01:50:19 +0900

    feat: Globally Manage Loading State for useAsync's IsLoading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,16 +2,20 @@ import Modal from 'react-modal';
 import { FormProvider, useForm } from 'react-hook-form';
 import FullLayout from '@/components/layout/Layouts/FullLayout';
 import EmptyLayout from '@/components/layout/Layouts/EmptyLayout';
+import useLoadingStore from '@/stores/useLoadingStore';
 import '@/styles/base/common.scss';
+import Spinner from '@/components/common/Spinner';
 
 Modal.setAppElement('#__next');
 
 export default function App({ Component, pageProps }) {
   const MainLayout = Component.FullLayout || EmptyLayout;
   const methods = useForm({ mode: 'all' });
+  const { isLoading } = useLoadingStore();
 
   return (
     <FormProvider {...methods}>
+      {isLoading && <Spinner />}
       <FullLayout>
         <MainLayout>
           <Component {...pageProps} />
---
+++ src/stores/useLoadingStore.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+const useLoadingStore = create((set) => ({
+  isLoading: false,
+  startLoading: () => set({ isLoading: true }),
+  stopLoading: () => set({ isLoading: false }),
+}));
+
+export default useLoadingStore;
