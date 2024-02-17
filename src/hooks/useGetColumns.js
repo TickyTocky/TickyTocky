c234a8786a1d384c86146cb08962bf52a7f7572a
designsoo <kheesoo10@gmail.com>
2024-02-18 00:20:56 +0900

    refactor: Add initial data for the column

--- src/hooks/useGetColumns.js
+++ src/hooks/useGetColumns.js
@@ -1,9 +1,10 @@
 import Columns from '@/api/columns';
 import useColumnStore from '@/stores/useColumnStore';
 import useAsync from '@/hooks/useAsync';
+import { INIT_COLUMNS_DATA } from '@/constants/initialDataType';
 
 const useGetColumns = ({ dashboardId }) => {
-  useAsync(() => Columns.getList(dashboardId));
+  useAsync(() => Columns.getList(dashboardId), INIT_COLUMNS_DATA);
   const { columnList } = useColumnStore();
 
   return { columnList };
